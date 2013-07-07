a20c03f6e00a0a7ee541eca68411c6b86e313656
SlyDeath <belyaev.rs@live.ru>
2013-07-07 21:15:18 +0400

    Update kladr.rb
    Minor fixes

--- kladr.rb
+++ kladr.rb
@@ -6,7 +6,8 @@ require 'dbf'
 require 'progressbar'
 
 def numeric? ( object )
-  true if Float(object) rescue false
+  return false unless object.to_s().strip =~ /(^[0-9\.]+$)/
+  true
 end
 
 def convert ( file )
@@ -23,8 +24,12 @@ def convert ( file )
   pbar = ProgressBar.new(file, rows)
 
   i = 0
+  k = 0
+  maxrecs = 1000
+  
   table.each do |rec|
     i += 1
+    k += 1
 
     count = rec.attributes.length
 
@@ -36,7 +41,11 @@ def convert ( file )
         prefix += '`' + key.downcase + '`' + ( qe = (q == count) ? '' : ', ' )
       end
 
-      File.open(sqlfile, 'w') { |file| file.write "INSERT INTO `#{sqlfile.gsub(/\.sql/i, '')}` (#{prefix}) VALUES \n" }
+      if ( k == 1 )
+        File.open(sqlfile, 'w') { |file| file.write "INSERT INTO `#{sqlfile.gsub(/.*?\//i, '').gsub(/\.sql/i, '')}` (#{prefix}) VALUES \n" }
+      else
+	      File.open(sqlfile, 'a') { |file| file.write "INSERT INTO `#{sqlfile.gsub(/.*?\//i, '').gsub(/\.sql/i, '')}` (#{prefix}) VALUES \n" }
+      end
     end
 
     File.open(sqlfile, 'a') { |file| file.write '(' }
@@ -45,12 +54,17 @@ def convert ( file )
     rec.attributes.each do |k, v|
       j += 1
 
-      v = ( v.nil? ) ? '""' : ( numeric?( v ) ) ? v : ( v.kind_of?(String) ) ? '"' + v + '"' : v
+      v = ( v.nil? ) ? '""' : ( numeric?( v ) ) ? v : ( v.kind_of?(String) ) ? '"' + v.gsub('"', '\"') + '"' : v
 
       File.open(sqlfile, 'a') { |file| file.write v.force_encoding("CP866").encode("UTF-8") + ( je = (j == count) ? '' : ', ' ) }
     end
 
-    File.open(sqlfile, 'a') { |file| file.write ')' + ( ie = (i == rows) ? ';' : ", \n" ) }
+    if ( i == maxrecs )
+      File.open(sqlfile, 'a') { |file| file.write ')' + ( ie = ";\n") }
+      i = 0
+    else
+      File.open(sqlfile, 'a') { |file| file.write ')' + ( ie = (k == rows) ? ';' : ", \n" ) }
+    end
     
     pbar.inc
   end
@@ -59,4 +73,3 @@ def convert ( file )
 end
 
 Dir.glob("./*.DBF") { |file|  convert ( file ) }
-
